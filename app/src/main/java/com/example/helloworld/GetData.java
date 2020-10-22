1460ae1322f31c3ed972fa0fa75885035cd47728
AlwyAbdullah <alwiabdullah4@gmail.com>
2020-10-22 11:52:53 +0700

    Get Data using Intent

--- app/src/main/java/com/example/helloworld/GetData.java
+++ app/src/main/java/com/example/helloworld/GetData.java
@@ -2,13 +2,21 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GetData extends AppCompatActivity {
+    int age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_data);
+
+        age = getIntent().getIntExtra("AGE", 20);
+        TextView tv = findViewById(R.id.textView4);
+
+        tv.setText(age);
     }
 }
